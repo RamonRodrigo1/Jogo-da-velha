754c697eb0c723ed1f6d5a78df348b5a9d152348
Ramon <ramon.rodrigo2014@hotmail.com>
2019-05-29 09:16:31 -0300

    adicionou-se a escolha da posição no tabuleiro

--- Jogo-da-velha/src/jogo/da/velha/Behavior.java
+++ Jogo-da-velha/src/jogo/da/velha/Behavior.java
@@ -10,35 +10,39 @@ import java.util.Scanner;
 
 public class Behavior {
     
-    private int[][] resultBoard = new int [3][3];
+    private int[][] gameBoard = new int [3][3];
     private boolean play = true;
     private boolean option = true;
     private boolean run = true;
     private boolean turn= true;
+    private int col;
+    private int row;
     private int play1;
     private int play2;
     private String choice;
     
     public void game(){
         
-        for(int i = 0; i < resultBoard.length; i++){
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i = 0; i < gameBoard.length; i++){
                     
-            for(int j = 0; j < resultBoard[0].length; j++){
+            for(int j = 0; j < gameBoard[0].length; j++){
                         
-                resultBoard[i][j] = 0;
+                gameBoard[i][j] = 0;
                         
             }
                 
         }
         
+        System.out.println("Bem-vindo ao Jogo da velha!");
+        
         while(run){
             
             if(option){
                 
-                System.out.println("Bem-vindo ao Jogo da velha!");
                 System.out.println("Jogador 1 escolha X ou O para começar.(Digite somenta x ou o)");
                 
-                Scanner sc = new Scanner(System.in);
                 choice = sc.next();
                 
                 if("X".equals(choice) || "x".equals(choice)){
@@ -58,54 +62,92 @@ public class Behavior {
             
             }
             
-            if(play){
-                
-                System.out.println("Jogo da Velha:");
-                
-                for(int i = 0; i < resultBoard.length; i++){
-                    
-                    for(int j = 0; j < resultBoard[0].length; j++){
-                        
-                        if(resultBoard[i][j] == 1){
-                            
-                            System.out.print("X");
-                        
-                        }if(resultBoard[i][j] == -1){
-                            
-                            System.out.print("O");
-                        
-                        }else{
-                            
-                            System.out.print(" ");
-                            
+            if(!option){
+            
+                if(play){
+
+                    System.out.println("Jogo da Velha:");
+
+                    for(int i = 0; i < gameBoard.length; i++){
+
+                        for(int j = 0; j < gameBoard[0].length; j++){
+
+                            if(gameBoard[i][j] == 1){
+
+                                System.out.print("X");
+
+                            }if(gameBoard[i][j] == -1){
+
+                                System.out.print("O");
+
+                            }else{
+
+                                System.out.print(" ");
+
+                            }
+
+                            if(j == 0 || j == 1){
+
+                                System.out.print("|");
+
+                            }else{
+
+                                System.out.println("");
+                                if(i == 0 ||i == 1){
+
+                                System.out.println("_____");
+
+                               }
+
+                            }
+
                         }
-                        
-                        if(j == 0 || j == 1){
-                            
-                            System.out.print("|");
-                        
-                        }else{
-                            
-                            System.out.println("");
-                            if(i == 0 ||i == 1){
-                            
-                            System.out.println("_____");
-                        
-                           }
-                            
-                        }
-                       
+
                     }
-                
-                }
-                
-                if(turn){
-                    
-                    System.out.println("Jogador 1 escolha sua posição.(linhas 1,2,3 colunas A,B,C)");
-                    
-                }
-                
-                run = false;
+
+                    System.out.println("DICA: Posição de tabuleiro.\n" +
+                                        "\n" +
+                                        " 1 2 3\n" +
+                                        "1X| |    11 x\n" +
+                                        "_______\n" +
+                                        "2 |O|    22 o\n" +
+                                        "_______\n" +
+                                        "3 |x|    23 x");
+
+                    if(turn){
+
+                        System.out.println("Jogador 1 escolha sua posição.");
+
+                        System.out.println("Coluna:");
+                        col = sc.nextInt() - 1;
+
+                        System.out.println("Linha:");
+                        row = sc.nextInt() - 1;
+                    }else{
+
+                        System.out.println("Jogador 2 escolha sua posição.");
+
+                        System.out.println("Coluna:");
+                        col = sc.nextInt() - 1;
+
+                        System.out.println("Linha:");
+                        row = sc.nextInt() - 1;
+
+                    }
+
+                    if(turn){
+
+                        gameBoard[row][col] = play1;
+                        turn = false;
+
+                    }else{
+
+                        gameBoard[row][col] = play2;
+                        turn = true;
+
+                    }
+                }    
+                //run = false;
             }
             
         }
